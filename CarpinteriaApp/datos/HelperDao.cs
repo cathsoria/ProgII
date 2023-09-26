aa7b571a688e36d0a90eefc1071de2e93845ce24
cathsoria <142896638+cathsoria@users.noreply.github.com>
2023-09-26 11:51:23 -0300

    "primer commit"

---
+++ CarpinteriaApp/datos/HelperDao.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarpinteriaApp.datos
+{
+    internal class HelperDao
+    {
+        private static HelperDao instancia;
+        private SqlConnection cnn;
+
+        private HelperDao()
+        {
+            cnn = new SqlConnection(Properties.Resources.cnnString);
+        }
+
+        public static HelperDao ObtenerInstancia()
+        {
+            if (instancia == null)
+                instancia = new HelperDao();
+            return instancia;
+        }
+
+        public int ConsultarEscalar(string nombreSP,string nombreParamOut)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cnn.Open();
+            cmd.Connection = cnn;
+            cmd.CommandText = nombreSP;
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlParameter pOut = new SqlParameter();
+            pOut.ParameterName = nombreParamOut;
+            pOut.DbType = DbType.Int32;
+            pOut.Direction = ParameterDirection.Output;
+            cmd.Parameters.Add(pOut);
+            cmd.ExecuteNonQuery();
+
+            cnn.Close();
+            return (int)pOut.Value;
+
+        }
+    }
+}
---
+++ CarpinteriaApp/datos/Implementacion/PresupuestoDao.cs
@@ -0,0 +1,42 @@
+﻿using CarpinteriaApp.datos.Interfaz;
+using CarpinteriaApp.dominio;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarpinteriaApp.datos.Implementacion
+{
+    internal class PresupuestoDao : IPresupuestoDao
+    {
+        public bool Crear(Presupuesto oPresupuesto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Producto> ObtenerProductos()
+        {
+            List<Producto> lProductos = new List<Producto>();
+            DataTable tabla = HelperDao.ObtenerInstancia().Consultar("SP_CONSULTAR_PRODUCTO");
+            foreach (DataRow fila in tabla.Rows)
+            {
+                int nro = int.Parse(fila["id_producto"].ToString());
+                string nom = fila["n_producto"].ToString();
+                double pre = double.Parse(fila["precio"].ToString());
+                bool activo = fila["activo"].ToString().Equals("S");
+                Producto p = new Producto(nro, nom, pre);
+                p.Activo = activo;
+                lProductos.Add(p);
+            }   
+
+            return lProductos;
+        }
+
+        public int ObtenerProximoPresupuesto()
+        {
+            return HelperDao.ObtenerInstancia().ConsultarEscalar("SP_PROXIMO_ID","@next");
+        }
+    }
+}
